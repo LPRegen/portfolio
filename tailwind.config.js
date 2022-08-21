f6d7c762520bf433ec79b1658e5c5348e5fffdf3
LPRegen <manuel.escribano.051@gmail.com>
2022-08-21 03:00:28 -0300

    feat: add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      'primary-bg': '#FFF7F0',
+      'hover-color': '#dfe0df',
+      'primary-text': '#2f4858',
+      'secondary-text': '#535c78',
+      'cta-color': '#D97738',
+    },
     extend: {},
   },
   plugins: [],
