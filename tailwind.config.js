e40e5790bb5d908cd0d2cc62710b4be165dc2b92
LPRegen <manuel.escribano.051@gmail.com>
2022-09-11 01:42:11 -0300

    refactor: modify color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      'primary-bg': '#FFF7F0',
-      'hover-color': '#dfe0df',
-      'primary-text': '#2f4858',
-      'secondary-text': '#535c78',
-      'cta-color': '#D97738',
+    extend: {
+      colors: {
+        primary: {
+          50: '#E4E3FF',
+          100: '#CBCDFF',
+          200: '#B2B7FF',
+          300: '#9AA1FF',
+          400: '#818CFF',
+          500: '#6778FF',
+          600: '#4B64EF',
+          700: '#2851D7',
+          800: '#003FC1',
+          900: '#002DAA',
+        },
+        secondary: {
+          50: '#F3F3F3',
+          100: '#DDDDDD',
+          200: '#C6C6C6',
+          300: '#B0B0B0',
+          400: '#9B9B9B',
+          500: '#868686',
+          600: '#727272',
+          700: '#5E5E5E',
+          800: '#4B4B4B',
+          900: '#5E5E5E',
+        },
+        black: '#1F1F1F',
+        alert: colors.red,
+        warning: colors.amber,
+        valid: colors.lime,
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
