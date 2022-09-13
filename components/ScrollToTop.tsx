c129a5ff34e90833fa9fcde4abad3531fb1ec84b
LPRegen <manuel.escribano.051@gmail.com>
2022-09-13 05:43:19 -0300

    refactor: add Icon component

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
-import { Icons } from './Icons';
+import { Icon } from './Icons';
 
 export default function ScrollToTop() {
   return (
     <Link href="/">
       <a>
-        <Icons
+        <Icon
           iconName="arrowUp"
           size="large"
-          className=" text-primary-500 rounded-lg bg-secondary-200 sticky top-[90%] ml-2"
+          className=" text-primary-500 rounded-lg bg-secondary-200 fixed bottom-4 right-4"
         />
       </a>
     </Link>
