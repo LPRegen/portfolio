de89188e53384465de1b40010d25236c4415b588
LPRegen <manuel.escribano.051@gmail.com>
2022-09-21 02:22:41 -0300

    refactor: refactor interface and add styles depending on as prop

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,13 +1,30 @@
 interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
   content: string;
-  as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  as: 'h1' | 'h2' | 'h3' | 'h4';
   className?: string;
 }
 
 export const Title = ({ content, as, className, ...props }: TitleProps) => {
   const Component = as;
+
+  let styles: string;
+  switch (as) {
+    case 'h1':
+      styles = 'font-extrabold text-2xl text-black';
+      break;
+    case 'h2':
+      styles = 'font-bold text-lg text-black';
+      break;
+    case 'h3':
+      styles = 'font-semibold text-lg text-black';
+      break;
+    case 'h4':
+      styles = 'font-bold text-base text-black';
+      break;
+  }
+
   return (
-    <Component className={className} {...props}>
+    <Component className={`${styles} ${className}`} {...props}>
       {content}
     </Component>
   );
