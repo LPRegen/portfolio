f50e0cc9ba1c7e50ec72935b57a2cbf07722f4f8
LPRegen <manuel.escribano.051@gmail.com>
2022-09-25 18:29:37 -0300

    refactor: modify styles

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -4,12 +4,8 @@ import { Icon } from './Icons';
 export default function ScrollToTop() {
   return (
     <Link href="/">
-      <a>
-        <Icon
-          iconName="arrowUp"
-          size="large"
-          className=" text-primary-500 rounded-lg bg-secondary-200 fixed bottom-4 right-4"
-        />
+      <a className="text-black text-opacity-[0.7] rounded-lg shadow-sm bg-secondary-100 fixed bottom-4 right-2">
+        <Icon iconName="arrowUp" size="large" />
       </a>
     </Link>
   );
