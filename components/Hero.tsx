e3fb30479feffea95023910d8c3e5174ae6876b8
LPRegen <manuel.escribano.051@gmail.com>
2022-09-26 07:37:44 -0300

    refactor: modify font class

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,13 +5,14 @@ export const Hero = () => {
   return (
     <div className="flex flex-col my-24">
       <Paragraph content="Hi, my name is" className="text-sm pb-4 opacity-80" />
-      <Title as="h1" className="text-[35px] pb-4">
+      <Title as="h1" className="text-hero pb-4">
         Manuel Escribano.
       </Title>
       <Paragraph
         content="I'm a self taught frontend developer looking to make a positive impact in the life of people through technology."
         className="text-sm opacity-80"
       />
+      {/* TODO Add call to action btn */}
     </div>
   );
 };
