ae684fb7e364dea46fb19cddfe3f1deafd5b5cc2
LPRegen <manuel.escribano.051@gmail.com>
2022-09-26 05:07:22 -0300

    refactor: modify component styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,10 @@ import { Paragraph } from './Paragraph';
 export const Hero = () => {
   return (
     <div className="flex flex-col my-24">
-      <Paragraph content="Hi, my name is" className="text-sm pb-2 opacity-80" />
-      <Title as="h1" content="Manuel Escribano." className="text-[35px] pb-2" />
+      <Paragraph content="Hi, my name is" className="text-sm pb-4 opacity-80" />
+      <Title as="h1" className="text-[35px] pb-4">
+        Manuel Escribano.
+      </Title>
       <Paragraph
         content="I'm a self taught frontend developer looking to make a positive impact in the life of people through technology."
         className="text-sm opacity-80"
