cbcf2e48093394105cad9503f2a019359f9f9997
LPRegen <manuel.escribano.051@gmail.com>
2022-09-28 01:56:21 -0300

    refactor: add project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,7 @@ import { Icon } from './Icons';
 import { SectionWrapper } from './Wrapper';
 
 import ImageTest from '../public/Screenshot_2022-08-26-01-49-11_3280x1080.png';
+import resumeBuilder from '../public/resume-builder.jpg';
 
 type Project = {
   name: string;
@@ -19,22 +20,23 @@ type Project = {
 
 const projectList: Project[] = [
   {
-    name: 'Todo List',
+    name: 'Resume builder',
     description:
-      'App to keep track of whatever you want, you will be able to categorize tasks by projects and set due dates for them, you will not miss a due date!',
-    technologies: 'Developed with JavaScript, Webpack and date-fns library.',
-    live: 'https:www.google.com',
-    repo: 'https:www.google.com',
-    image: ImageTest,
-    altImg: 'test',
+      'With this app you will be able to create your own resume based on a predefined template, at the end you can download it as a pdf file.',
+    technologies:
+      'React.js React-icons react-router-dom react-to-print Styled Components',
+    live: 'https://resume-builder-lime.vercel.app/',
+    repo: 'https://github.com/LPRegen/resume-builder',
+    image: resumeBuilder,
+    altImg: 'Resume builder mockup',
   },
   {
-    name: 'Todo List2',
+    name: 'Weather App',
     description:
-      'App to keep track of whatever you want, you will be able to categorize tasks by projects and set due dates for them, you will not miss a due date!',
-    technologies: 'Developed with JavaScript, Webpack and date-fns library.',
-    live: 'https:www.google.com',
-    repo: 'https:www.google.com',
+      'You will be able to find the forecast for a specific city. The used API is from OpenWeather. The main purpouse was to practice API calls and use asynchronous code with promises.',
+    technologies: 'JS Webpack SASS',
+    live: 'https://lpregen.github.io/weather-app/',
+    repo: 'https://github.com/LPRegen/weather-app',
     image: ImageTest,
     altImg: 'test',
   },
@@ -73,7 +75,7 @@ export const Projects = () => {
     <SectionWrapper id="projects" title="Projects">
       {projectList.map((project) => {
         return (
-          <div key={project.name} className="shadow-xl rounded-xl">
+          <div key={project.name} className="shadow-2xl rounded-xl">
             <Image
               src={project.image}
               alt={project.altImg}
@@ -87,7 +89,7 @@ export const Projects = () => {
               <Paragraph className="text-secondary-800">
                 {project.description}
               </Paragraph>
-              <Paragraph className="text-secondary-700">
+              <Paragraph className="text-secondary-600 w-full">
                 {project.technologies}
               </Paragraph>
               <Links repo={project.repo} live={project.live} />
