80caabd32dea99ab5232821350c018dda08bb318
LPRegen <manuel.escribano.051@gmail.com>
2022-09-29 14:15:59 -0300

    refactor: test layer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,24 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
-import { Title } from '../components/Heading';
+import { Footer } from '../components/Footer';
+import { AboutMe } from '../components/About';
+import Navbar from '../components/Navbar';
+import { Projects } from '../components/Projects';
+import { ContactForm } from '../components/Contact';
+import ScrollToTop from '../components/ScrollToTop';
+import { Hero } from '../components/Hero';
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="bg-black/60 h-[100vh] flex flex-col gap-16 justify-center items-center">
-        <Title
-          as="h1"
-          content={'⚙️ Under construction ⚙️'}
-          className="text-2xl"
-        />
-        <span className="text-lg">
-          Find me on{' '}
-          <Link href={'https://www.linkedin.com/in/manuel-escribano-lpregen/'}>
-            <a className="text-white underline underline-offset-2">LinkedIn</a>
-          </Link>{' '}
-          or check out my{' '}
-          <Link href={'https://github.com/LPRegen'}>
-            <a className="text-white underline underline-offset-2">GitHub</a>
-          </Link>
-        </span>
+      <Navbar />
+      <div className="flex flex-col gap-32 mx-12 pt-24 sm:mx-24 md:mx-36">
+        <Hero />
+        <AboutMe />
+        <Projects />
+        <ContactForm />
       </div>
+      <Footer />
+      <ScrollToTop />
     </>
   );
 };
