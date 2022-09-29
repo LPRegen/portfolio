f6095d38cd07d53726f04f9c0fa34f5fa05396ff
LPRegen <manuel.escribano.051@gmail.com>
2022-09-29 05:29:52 -0300

    refactor: add Link to contact section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import { Title } from './Heading';
 import { Paragraph } from './Paragraph';
 
 export const Hero = () => {
+  const contactStyles =
+    'bg-primary-200/80 rounded-lg px-4 py-2 text-lg mr-auto my-6 hover:bg-primary-500/70 focus:outline-none focus:ring focus:ring-primary-400';
+
   return (
-    <div className="flex flex-col my-24">
+    <div className="flex flex-col my-32">
       <Paragraph className="text-sm pb-4 opacity-80">Hi, my name is</Paragraph>
       <Title as="h1" className="text-hero pb-4">
         Manuel Escribano.
@@ -12,7 +16,11 @@ export const Hero = () => {
         I am a self taught frontend developer looking to make a positive impact
         in the life of people through technology.
       </Paragraph>
-      {/* TODO Add call to action btn */}
+      <div className="flex gap-8 flex-col">
+        <Link href="#contact">
+          <a className={contactStyles}>Contact me</a>
+        </Link>
+      </div>
     </div>
   );
 };
