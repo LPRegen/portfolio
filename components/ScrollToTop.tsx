3ff0b94158f7aa00446162ba62b6c52e3b2bf67c
LPRegen <manuel.escribano.051@gmail.com>
2022-09-30 13:09:53 -0300

    refactor: add styles for diff screen resolutions

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link';
 import { Icon } from './Icons';
 
 export default function ScrollToTop() {
+  const scrollStyles =
+    'text-black text-opacity-[0.7] rounded-lg shadow-sm bg-secondary-100 fixed bottom-4 right-2 sm:right-6 sm:p-3';
+
   return (
     <Link href="/">
-      <a className="text-black text-opacity-[0.7] rounded-lg shadow-sm bg-secondary-100 fixed bottom-4 right-2">
+      <a className={`${scrollStyles}`}>
         <Icon iconName="arrowUp" size="large" />
       </a>
     </Link>
