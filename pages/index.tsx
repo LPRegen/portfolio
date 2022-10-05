6e3e40ef885688807dce48d05f1ca76d0b66dcb8
LPRegen <manuel.escribano.051@gmail.com>
2022-10-05 14:50:54 -0300

    refactor: modify margin on different screen resolutions

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col gap-32 mx-12 pt-24 sm:mx-24 md:mx-36 lg:mx-64 xl:mx-80 2xl:mx-96">
+      <div className="flex flex-col gap-44 mx-8 pt-24 sm:mx-16 sm:gap-56 md:mx-28 md:gap-64 lg:mx-36 lg:gap-80 xl:mx-56 2xl:mx-64">
         <Hero />
         <AboutMe />
         <Projects />
