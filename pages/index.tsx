2f14c1f21045d2eb72029bad0e6ab6daa0629f4a
LPRegen <manuel.escribano.051@gmail.com>
2022-10-05 23:47:05 -0300

    feat: create loader component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
 import type { NextPage } from 'next';
-import { Footer } from '../components/Footer';
-import { AboutMe } from '../components/About';
+import { useEffect, useState } from 'react';
+
+import { Loader } from '../components/Loader';
 import Navbar from '../components/Navbar';
+import { Hero } from '../components/Hero';
+import { AboutMe } from '../components/About';
 import { Projects } from '../components/Projects';
 import { ContactForm } from '../components/Contact';
 import ScrollToTop from '../components/ScrollToTop';
-import { Hero } from '../components/Hero';
+import { Footer } from '../components/Footer';
 
 const Home: NextPage = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  const handleLoading = () => {
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (document.readyState === 'complete') {
+      handleLoading();
+    } else {
+      window.addEventListener('load', handleLoading);
+    }
+    return () => window.removeEventListener('load', handleLoading);
+  }, []);
+
+  const mainWrapper =
+    'flex flex-col gap-44 mx-8 pt-24 sm:mx-16 sm:gap-56 md:mx-28 md:gap-64 lg:mx-36 lg:gap-80 xl:mx-56 2xl:mx-64';
+
+  return loading ? (
+    <Loader />
+  ) : (
     <>
       <Navbar />
-      <div className="flex flex-col gap-44 mx-8 pt-24 sm:mx-16 sm:gap-56 md:mx-28 md:gap-64 lg:mx-36 lg:gap-80 xl:mx-56 2xl:mx-64">
+      <div className={mainWrapper}>
         <Hero />
         <AboutMe />
         <Projects />
