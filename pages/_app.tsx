5f4c885c2657f1c5c3a04727f91303daec06d747
LPRegen <manuel.escribano.051@gmail.com>
2022-10-08 02:01:17 -0300

    refactor: remove loader component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,10 @@
 import '../styles/globals.css';
-import { useState, useEffect } from 'react';
 import type { AppProps } from 'next/app';
-import { BlocksWave } from 'react-svg-spinners';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(true);
-
-  const handleLoading = () => {
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    if (document.readyState === 'complete') {
-      handleLoading();
-    } else {
-      window.addEventListener('load', handleLoading);
-    }
-    return () => window.removeEventListener('load', handleLoading);
-  }, []);
-
   return (
     <>
-      {!loading ? (
-        <Component {...pageProps} />
-      ) : (
-        <div className="grid">
-          <BlocksWave />
-        </div>
-      )}
+      <Component {...pageProps} />
     </>
   );
 }
