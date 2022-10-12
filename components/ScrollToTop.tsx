d2b1ed71d5821322722d88d554e7d243b57b4979
LPRegen <manuel.escribano.051@gmail.com>
2022-10-12 03:40:20 -0300

    refactor: display comp depending on sroll position

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Icon } from './Icons';
 
 export default function ScrollToTop() {
-  const scrollStyles =
-    'text-black text-opacity-[0.7] rounded-lg shadow-sm bg-secondary-100 fixed bottom-4 right-2 sm:right-4 md:right-6 md:p-3 lg:right-9';
+  const [showScrollBtn, setShowScrollBtn] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 210) {
+        setShowScrollBtn(true);
+      } else {
+        setShowScrollBtn(false);
+      }
+    });
+  }, []);
+
+  const scrollStyles = `text-black text-opacity-[0.7] rounded-lg shadow-sm bg-secondary-100 bottom-4 right-2 sm:right-4 md:right-6 md:p-3 lg:right-9 ${
+    showScrollBtn ? 'fixed' : 'hidden aria-hidden'
+  }`;
 
   return (
     <Link href="/">
