6ce51ee60fd546068641c5f66be6a20c2bb07eb7
LPRegen <manuel.escribano.051@gmail.com>
2022-10-17 09:13:01 -0300

    refactor: add types

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,19 +5,20 @@ import { Paragraph } from './Paragraph';
 import { Icon } from './Icons';
 import Link from 'next/link';
 
-export function ContactForm() {
+export function ContactForm(): JSX.Element {
   const [state, handleSubmit] = useForm('mnqwwopk');
   const containerStyle = 'grid gap-2';
 
-  const ContactButton = () => {
+  const ContactButton = (): JSX.Element => {
     let formState = 'Submit';
     if (state.submitting) {
       formState = 'Sending...';
     } else if (state.succeeded) {
       formState = 'Sended!';
     }
-    const sendingStyle = () => (state.submitting ? 'bg-primary-400/60' : '');
-    const buttonStyle = `${sendingStyle()} rounded-lg text-secondary-800 bg-primary-200/50 p-4 disabled:bg-lime-200/60 disabled:text-primary-700 disabled:cursor-not-allowed`;
+    const sendingStyle = (): string =>
+      state.submitting ? 'bg-primary-400/60' : '';
+    const buttonStyle: string = `${sendingStyle()} rounded-lg text-secondary-800 bg-primary-200/50 p-4 disabled:bg-lime-200/60 disabled:text-primary-700 disabled:cursor-not-allowed`;
 
     return (
       <Button
@@ -31,17 +32,15 @@ export function ContactForm() {
     );
   };
 
-  const Legend = (): JSX.Element => {
-    return (
-      <Paragraph className="max-w-3xl xl:ml-8 2xl:ml-12">
-        Currently I am looking for a position as a frontend developer, if you
-        have any question or just want to chat feel free to send me a message
-        and I will get back to you soon as I can!
-      </Paragraph>
-    );
-  };
+  const Legend = (): JSX.Element => (
+    <Paragraph className="max-w-3xl xl:ml-8 2xl:ml-12">
+      Currently I am looking for a position as a frontend developer, if you have
+      any question or just want to chat feel free to send me a message and I
+      will get back to you soon as I can!
+    </Paragraph>
+  );
 
-  const Form = () => {
+  const Form = (): JSX.Element => {
     const labelStyle = 'text-sm text-secondary-900';
     const inputStyle =
       'px-1 py-2 text-base border border-primary-300/50 focus:outline-none focus:border-primary-500';
