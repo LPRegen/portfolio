fff06b622e5fcde25c266abb3eb5181d5a64df9b
LPRegen <manuel.escribano.051@gmail.com>
2022-10-21 15:33:47 -0300

    refactor: add cleanup function on useEffect

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -5,14 +5,12 @@ import { Icon } from './Icons';
 export default function ScrollToTop() {
   const [showScrollBtn, setShowScrollBtn] = useState<boolean>(false);
 
+  const checkScroll = () =>
+    window.scrollY > 210 ? setShowScrollBtn(true) : setShowScrollBtn(false);
+
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 210) {
-        setShowScrollBtn(true);
-      } else {
-        setShowScrollBtn(false);
-      }
-    });
+    window.addEventListener('scroll', checkScroll);
+    return () => window.removeEventListener('scroll', checkScroll);
   }, []);
 
   const scrollStyles = `text-black text-opacity-[0.7] rounded-lg shadow-sm bg-secondary-100 bottom-4 right-2 sm:right-4 md:right-6 md:p-3 lg:right-9 ${
