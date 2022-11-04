fb3ebf7969a9476ef57579ae3c16d978a48bc01b
LPRegen <manuel.escribano.051@gmail.com>
2022-11-04 06:21:54 -0300

    refactor: render icons

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,14 +1,15 @@
 import { Paragraph } from './Paragraph';
+import { Icon } from './Icons';
 
 interface TechnologiesProps {
   title: string;
-  icon: React.ReactElement;
+  icon: string;
 }
 
 export const Technology = ({ title, icon }: TechnologiesProps) => {
   return (
     <div className="flex flex-col gap-2 justify-center items-center text-secondary-800">
-      {icon}
+      <Icon iconName={icon} aria-hidden />
       <Paragraph className="text-secondary-600 text-xs font-lato">
         {title}
       </Paragraph>
