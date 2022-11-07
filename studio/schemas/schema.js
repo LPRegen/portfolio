a524b79424310db2455d184136b0c60592246764
LPRegen <manuel.escribano.051@gmail.com>
2022-11-07 03:46:40 -0300

    feat: create blog schemas

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,8 +1,15 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+// schemas
+import category from './category';
+import post from './post';
+import author from './author';
+import comment from './comment';
+import blockContent from './blockContent';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,5 +19,10 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    category,
+    post,
+    author,
+    comment,
+    blockContent,
   ]),
-})
+});
