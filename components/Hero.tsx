9328427d5b6259c4ed4e3442d87dfe0f11317590
LPRegen <manuel.escribano.051@gmail.com>
2022-11-24 14:36:52 -0300

    refactor: modify text content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,12 @@
-import { CustomLink } from "./CustomLink";
-import { Heading } from "./Heading";
-import { Paragraph } from "./Paragraph";
+import { CustomLink } from './CustomLink';
+import { Heading } from './Heading';
+import { Paragraph } from './Paragraph';
 
 export const Hero = () => {
-  const heroStyles = "flex flex-col gap-4 my-32 sm:pt-32 sm:gap-6";
-  const textStyles = "opacity-80";
+  const heroStyles = 'flex flex-col gap-4 my-32 sm:pt-32 sm:gap-6';
+  const textStyles = 'opacity-80';
   const contactStyles =
-    "bg-primary-200/80 rounded-lg px-4 py-2 text-lg mr-auto my-6 hover:bg-primary-500/70 focus:outline-none focus:ring focus:ring-primary-400";
+    'bg-primary-200/80 rounded-lg px-4 py-2 text-lg mr-auto my-6 hover:bg-primary-500/70 focus:outline-none focus:ring focus:ring-primary-400';
 
   return (
     <div className={heroStyles}>
@@ -15,13 +15,13 @@ export const Hero = () => {
         Manuel Escribano
       </Heading>
       <Paragraph className={`${textStyles} max-w-[740px]`}>
-        Self taught frontend developer looking to make a positive impact in the
-        life of people through technology.
+        Self taught frontend developer making a positive impact in the life of
+        people through technology.
       </Paragraph>
       <CustomLink
         href="#contact"
         className={`${contactStyles} font-lato`}
-        title={"Contact me"}
+        title={'Contact me'}
       >
         Contact me
       </CustomLink>
