2253b629121a9228493fd2f2bf0644233a3ed507
LPRegen <manuel.escribano.051@gmail.com>
2022-11-30 04:04:54 -0300

    feat: create profile component

---
+++ components/Profile.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import urlForImage from '../studio/lib/utils';
+import type { Author } from '../types/schema-types';
+
+interface ProfileProps extends Omit<Author, 'bio'> {
+  lastUpdate: string;
+}
+
+export const Profile = ({ name, image, lastUpdate }: ProfileProps) => {
+  return (
+    <div className="flex items-center justify-start">
+      <div className="relative h-10 w-10">
+        <Image
+          alt={'Manuel Escribano is picture'}
+          src={urlForImage(image.asset._ref).url()}
+          className={`object-cover rounded-full`}
+          fill
+        />
+      </div>
+      <div className="grid ml-3">
+        <p className="text-orange-700">{name}</p>
+        <p className="font-light text-xs">
+          {/* TODO Refactor date */}
+          Last update:{' '}
+          {new Date(lastUpdate).toLocaleDateString('en-US', {
+            month: 'long',
+            year: 'numeric',
+          })}
+        </p>
+      </div>
+    </div>
+  );
+};
