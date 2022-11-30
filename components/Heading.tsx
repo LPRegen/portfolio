b7644a55b750e73e727d4fef53c23be3459b133f
LPRegen <manuel.escribano.051@gmail.com>
2022-11-30 05:41:01 -0300

    refactor: change font sizes

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,6 +1,6 @@
 interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
   children: React.ReactNode;
-  as: "h1" | "h2" | "h3" | "h4";
+  as: 'h1' | 'h2' | 'h3' | 'h4';
   className?: string;
   after?: boolean;
 }
@@ -8,7 +8,7 @@ interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
 export const Heading = ({
   children,
   as,
-  className = "",
+  className = '',
   after,
   ...props
 }: TitleProps) => {
@@ -16,23 +16,23 @@ export const Heading = ({
 
   let styles: string;
   switch (as) {
-    case "h1":
-      styles = "font-extrabold text-2xl text-black";
+    case 'h1':
+      styles = 'font-extrabold text-4xl font-lato';
       break;
-    case "h2":
-      styles = "font-bold text-xl text-black font-lato ";
+    case 'h2':
+      styles = 'font-bold text-3xl font-lato ';
       break;
-    case "h3":
-      styles = "font-semibold text-lg text-black font-lato ";
+    case 'h3':
+      styles = 'font-semibold text-2xl font-lato ';
       break;
-    case "h4":
-      styles = "font-bold text-base text-black font-lato ";
+    case 'h4':
+      styles = 'font-bold text-xl font-lato ';
       break;
   }
 
   const afterLine = after
-    ? 'after:content-[""] after:block after:relative after:border-b after:w-[60%] after:top-[-0.8em] after:right-0 after:ml-[40%] after:mr-[5%] after:border-secondary-400'
-    : "";
+    ? 'after:content-[""] after:block after:relative after:border-b after:w-[50%] after:top-[-0.6em] after:right-0 after:ml-[50%] after:mr-[5%] after:border-secondary-400'
+    : '';
 
   return (
     <Component
