9b2722f79b3153f53dfdc1dba1e45a9df45e9024
LPRegen <manuel.escribano.051@gmail.com>
2022-11-29 23:05:11 -0300

    refactor: modify imports

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,27 +1,29 @@
-import { useEffect } from 'react';
 import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.min.css';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/themes/prism-tomorrow.min.css';
+import { useEffect } from 'react';
 
 interface CodeProps {
-  language: string;
+  language?: string;
   code: string;
 }
 
 export const Code = ({ language, code }: CodeProps) => {
-  require(`prismjs/components/prism-${language}`);
-
+  if (language === 'tsx') {
+    require(`prismjs/components/prism-jsx`);
+    require(`prismjs/components/prism-tsx`);
+  } else {
+    require(`prismjs/components/prism-${language}`);
+  }
   useEffect(() => {
     Prism.highlightAll();
   }, []);
 
   return (
     <div className="bg-red-200">
-      {/* TODO Display highlighted lines */}
-      <div>Just asd another code block</div>
       <pre className="line-numbers">
-        <code className={`language-${language}`}>{code}</code>
+        <code className={`lang-${language}`}>{code}</code>
       </pre>
       <p>Language: {language}</p>
     </div>
