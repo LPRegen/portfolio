89a699333907dd16a67f38a3b0ff4890b8174136
LPRegen <manuel.escribano.051@gmail.com>
2022-12-05 02:38:37 -0300

    refactor: modify component color

--- components/Bold.tsx
+++ components/Bold.tsx
@@ -4,6 +4,6 @@ interface BoldProps {
 }
 
 export const Bold = ({ children, className = '' }: BoldProps) => {
-  const spanStyles = 'text-primary-800/90 font-medium';
+  const spanStyles = 'text-orange-700 font-medium';
   return <span className={`${spanStyles} ${className}`}>{children}</span>;
 };
