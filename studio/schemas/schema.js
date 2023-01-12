a201b0b2302aab0b2e05e1e90cf09b2668b6e672
LPRegen <manuel.escribano.051@gmail.com>
2023-01-12 15:50:59 -0300

    refactor: add project schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,18 +1,20 @@
-import createSchema from 'part:@sanity/base/schema-creator';
-import schemaTypes from 'all:part:@sanity/base/schema-type';
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import category from './category';
-import post from './post';
-import comment from './comment';
-import blockContent from './blockContent';
+import category from "./category";
+import post from "./post";
+import comment from "./comment";
+import blockContent from "./blockContent";
+import project from "./projects";
 
 export default createSchema({
-  name: 'default',
+  name: "default",
   types: schemaTypes.concat([
     /* Your types here! */
     category,
     post,
     comment,
     blockContent,
+    project,
   ]),
 });
