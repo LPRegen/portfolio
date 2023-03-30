823a1a1682f4f3ffbe82528367cea9576675c76c
LPRegen <manuel.escribano.051@gmail.com>
2023-03-30 12:49:46 +0200

    refactor: add helper functions

--- schemas/project.ts
+++ schemas/project.ts
@@ -1,62 +1,66 @@
-export default {
+import { defineArrayMember, defineField, defineType } from "sanity";
+
+const project = defineType({
   title: "Project List",
   type: "document",
   name: "projectList",
+  preview: { select: { title: "projTitle" } },
   fields: [
-    {
+    defineField({
       title: "Project Title",
       type: "string",
       name: "projTitle",
-    },
-    {
+    }),
+    defineField({
       title: "Description",
       type: "array",
       name: "projDescription",
-      of: [{ type: "block" }],
-    },
-    {
+      of: [defineArrayMember({ type: "block" })],
+    }),
+    defineField({
       title: "Technologies",
       type: "array",
       name: "techList",
-      of: [{ type: "string" }],
+      of: [defineArrayMember({ type: "string" })],
       options: {
         layout: "tags",
       },
-    },
-    {
+    }),
+    defineField({
       title: "URLs",
       name: "urlsGroup",
       type: "object",
       fields: [
-        {
+        defineField({
           title: "Repository URL",
           name: "repoUrl",
           type: "url",
-        },
-        {
+        }),
+        defineField({
           title: "Live URL",
           name: "liveUrl",
           type: "url",
-        },
+        }),
       ],
-    },
-    {
+    }),
+    defineField({
       title: "Project Image",
       name: "imgGroup",
       type: "object",
       fields: [
-        {
+        defineField({
           title: "Image",
           name: "projImage",
           type: "image",
-        },
-        {
+        }),
+        defineField({
           title: "Alternative Text for Image",
           name: "altImage",
           type: "string",
-        },
+        }),
       ],
-    },
+    }),
   ],
-  preview: { select: { title: "projTitle" } },
-};
+});
+
+export default project;
