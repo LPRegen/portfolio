61eba5869eec3db1527c2f2390c582f23209bda0
LPRegen <manuel.escribano.051@gmail.com>
2023-03-31 10:06:17 +0200

    refactor: modify import statement

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "sanity";
 import { dataset, projectId, title } from "./lib/sanity-variables";
 import { visionTool } from "@sanity/vision";
 import { deskTool } from "sanity/desk";
-import schemas from "schemas";
+import schemas from "./schemas";
 
 export default defineConfig({
   basePath: "/studio",
