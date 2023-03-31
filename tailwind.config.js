c597bfdbd86b6f1464d8df0eed12a79993f1db10
LPRegen <manuel.escribano.051@gmail.com>
2023-03-31 09:21:08 +0200

    refactor: remove plugins and modify content config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,46 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontSize: {
-        hero: 'clamp(1.5rem, 1.8852rem + 1.3456vw, 3.5rem)',
+        hero: "clamp(1.5rem, 1.8852rem + 1.3456vw, 3.5rem)",
       },
       colors: {
         primary: {
-          50: '#E4E3FF',
-          100: '#CBCDFF',
-          200: '#B2B7FF',
-          300: '#9AA1FF',
-          400: '#818CFF',
-          500: '#6778FF',
-          600: '#4B64EF',
-          700: '#2851D7',
-          800: '#003FC1',
-          900: '#002DAA',
+          50: "#E4E3FF",
+          100: "#CBCDFF",
+          200: "#B2B7FF",
+          300: "#9AA1FF",
+          400: "#818CFF",
+          500: "#6778FF",
+          600: "#4B64EF",
+          700: "#2851D7",
+          800: "#003FC1",
+          900: "#002DAA",
         },
         secondary: {
-          50: '#F3F3F3',
-          100: '#DDDDDD',
-          200: '#C6C6C6',
-          300: '#B0B0B0',
-          400: '#9B9B9B',
-          500: '#868686',
-          600: '#727272',
-          700: '#5E5E5E',
-          800: '#4B4B4B',
-          900: '#5E5E5E',
+          50: "#F3F3F3",
+          100: "#DDDDDD",
+          200: "#C6C6C6",
+          300: "#B0B0B0",
+          400: "#9B9B9B",
+          500: "#868686",
+          600: "#727272",
+          700: "#5E5E5E",
+          800: "#4B4B4B",
+          900: "#5E5E5E",
         },
-        black: '#111827',
+        black: "#111827",
         alert: colors.red,
         warning: colors.amber,
         valid: colors.lime,
       },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp')],
 };
