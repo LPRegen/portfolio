02d8627e3df46d2998ec502fb9e71b6c0055d4f2
LPRegen <manuel.escribano.051@gmail.com>
2023-03-31 13:07:26 +0200

    refactor: add helper functions

--- schemas/post.ts
+++ schemas/post.ts
@@ -1,22 +1,21 @@
-import { DocumentIcon } from "@sanity/icons";
+import { defineArrayMember, defineField, defineType } from "sanity";
 
-export default {
-  name: "post",
+const post = defineType({
   title: "Post",
-  icon: DocumentIcon,
   type: "document",
+  name: "post",
   fields: [
-    {
+    defineField({
       name: "title",
       title: "Title",
       type: "string",
-    },
-    {
+    }),
+    defineField({
       name: "description",
       title: "Post description",
       type: "string",
-    },
-    {
+    }),
+    defineField({
       name: "slug",
       title: "Slug",
       type: "slug",
@@ -24,54 +23,57 @@ export default {
         source: "title",
         maxLength: 96,
       },
-    },
-    {
+    }),
+    defineField({
       name: "postImage",
       title: "Post image",
       type: "object",
       fields: [
-        {
+        defineField({
           name: "imageUrl",
           title: "Image URL",
           type: "string",
-        },
-        {
+        }),
+        defineField({
           name: "authorName",
           title: "Author name",
           type: "string",
-        },
-        {
+        }),
+        defineField({
           name: "authorProfile",
           title: "Author profile",
           type: "string",
-        },
-        {
+        }),
+        defineField({
           name: "altTextImage",
           title: "Img alternative text",
           type: "string",
-        },
+        }),
       ],
-    },
-    {
+    }),
+    defineField({
       name: "categories",
       title: "Categories",
       type: "array",
-      of: [{ type: "reference", to: { type: "category" } }],
-    },
-    {
+      of: [defineArrayMember({ type: "reference", to: { type: "category" } })],
+    }),
+    defineField({
       title: "Body",
       name: "body",
       type: "array",
-      of: [{ type: "block" }, { name: "codeBlock", type: "code" }],
-    },
-    {
+      of: [
+        defineArrayMember({ type: "block" }),
+        defineField({ name: "codeBlock", type: "code" }),
+      ],
+    }),
+    defineField({
       name: "publishedAt",
       title: "Published at",
       type: "date",
       options: {
         dateFormat: "MMM-D-YYYY",
       },
-    },
+    }),
   ],
 
   preview: {
@@ -87,4 +89,6 @@ export default {
       });
     },
   },
-};
+});
+
+export default post;
