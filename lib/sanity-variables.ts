2296dbeb12f4e3c6746ced050b369cc5acefa2ae
LPRegen <manuel.escribano.051@gmail.com>
2023-04-03 01:10:15 +0200

    refactor: modify apiVersion date

--- lib/sanity-variables.ts
+++ lib/sanity-variables.ts
@@ -2,6 +2,6 @@ const title = process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE || "LPRegen-blog";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "ci2u4db7";
 const token = process.env.NEXT_PUBLIC_SANITY_API_READ_TOKEN;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2022-11-16";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-04-02";
 
 export { apiVersion, dataset, projectId, title, token };
