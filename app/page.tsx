7402daeade14d03f94372b7e820ba9574fb25c6b
LPRegen <manuel.escribano.051@gmail.com>
2023-04-07 16:44:14 +0200

    refactor: change html container tag

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import { Projects } from "../components/Projects";
 
 function Home() {
   return (
-    <div className="flex flex-col gap-44 mx-8 pt-24 sm:mx-16 sm:gap-56 md:mx-28 md:gap-64 lg:mx-36 lg:gap-80 xl:mx-56 2xl:mx-64">
+    <main className="flex flex-col gap-44 mx-8 pt-24 sm:mx-16 sm:gap-56 md:mx-28 md:gap-64 lg:mx-36 lg:gap-80 xl:mx-56 2xl:mx-64">
       <Hero />
       <AboutMe />
       <Projects />
       <ContactForm />
-    </div>
+    </main>
   );
 }
 
