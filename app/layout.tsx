3c6a970c221b26bc0d7d8c79179eb6913164debf
LPRegen <manuel.escribano.051@gmail.com>
2023-04-10 14:43:37 +0200

    feat: add static metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,39 @@
+import { Metadata } from "next";
 import { Footer } from "components/Footer";
-import { HeadComp } from "components/HeadComp";
 import Navbar from "components/Navbar";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Manuel Escribano, Frontend web developer",
+  applicationName: `LPRegen's portfolio`,
+  keywords: [
+    "Manuel",
+    "Escribano",
+    "Frontend",
+    "Web",
+    "Developer",
+    "Desarrollador",
+    "Freelance",
+    "html",
+    "css",
+    "javascript",
+    "react",
+    "js",
+    "tailwind",
+    "typescript",
+    "ts",
+    "next",
+  ],
+  authors: [{ name: "Manuel Escribano" }],
+  creator: "Manuel Escribano",
+  publisher: "Manuel Escribano",
+};
 
 export default function RootLayout({
   children,
 }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <HeadComp />
       <body>
         <Navbar />
         {children}
