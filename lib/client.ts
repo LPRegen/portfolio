848dda80fb488eff95dfa1613d1d4ddb45e4c305
LPRegen <manuel.escribano.051@gmail.com>
2023-05-24 05:16:12 +0200

    refactor: fix urlFor with Sanity client

---
+++ lib/client.ts
@@ -0,0 +1,16 @@
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { createClient, SanityClient } from "next-sanity";
+import { apiVersion, dataset, projectId, token } from "./sanity-variables";
+
+export const client: SanityClient = createClient({
+  apiVersion,
+  dataset,
+  projectId,
+  token,
+  useCdn: true,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source: SanityImageSource) => builder.image(source);
--- lib/urlFor.ts
+++
@@ -1,9 +0,0 @@
-import client from "../sanity.config";
-import imageUrlBuilder from "@sanity/image-url";
-import { Image } from "../types/schema-types";
-
-const builder = imageUrlBuilder(client);
-
-export default function urlFor(source: Image) {
-  return builder.image(source);
-}
