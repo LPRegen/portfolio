d898582df3f66c2e4fa97bc847d0c3404aaad0f1
LPRegen <manuel.escribano.051@gmail.com>
2023-05-24 04:55:01 +0200

    refactor: add styles for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,12 +31,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: { children: React.ReactNode }) {
+}: {
+  children: React.ReactNode;
+}) {
+  const mainStyle = "mx-4 max-w-4xl sm:mx-8 lg:mx-auto";
   return (
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <main className={mainStyle}>{children}</main>
         <Footer />
       </body>
     </html>
