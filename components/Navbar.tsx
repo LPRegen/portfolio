ee2057fc70b729b9c1739554386b5f981ee5233d
LPRegen <manuel.escribano.051@gmail.com>
2023-05-26 13:45:38 +0200

    refactor: modify component style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,17 +11,14 @@ const itemList: Array<Url> = [
 ];
 
 const navItem =
-  "block px-3 py-2 my-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:bg-gray-200/80 focus:bg-gray-200/80";
+  "flex items-center p-1.5 my-1 font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:bg-gray-200/80 focus:bg-gray-200/80";
 
-const wrapperStyle = "absolute w-full z-50 border-b shadow-lg";
+const containerStyle =
+  "flex gap-8 h-14 px-4 border-b shadow-lg sm:px-8 lg:px-14";
 
-// FIX: display
 const Navbar = () => {
-  const mdStyles = "md:gap-8 md:flex md:items-center md:px-28";
   return (
-    <nav
-      className={`hidden h-14 ${wrapperStyle} ${mdStyles} lg:px-36 xl:px-56 2xl:px-64`}
-    >
+    <nav className={containerStyle}>
       {itemList.map((item) => (
         <CustomLink
           key={item.name}
