444761e5c78b6190cd71b4bb8247888e380fd7ea
LPRegen <manuel.escribano.051@gmail.com>
2023-06-02 17:03:34 +0200

    refactor: move blog route into web group

--- app/(web)/blog/layout.tsx
+++ app/(web)/blog/layout.tsx
@@ -1,19 +1,14 @@
-import { Footer } from "components/Footer";
-import Navbar from "components/Navbar";
-
 export default async function BlogLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const mainStyle =
-    "grid grid-rows-auto gap-16 mx-4 max-w-3xl sm:mx-8 md:mx-auto";
+    "grid grid-rows-auto mb-20 gap-16 mx-4 max-w-3xl sm:mx-8 md:mx-auto";
 
   return (
     <>
-      <Navbar />
       <main className={mainStyle}>{children}</main>
-      <Footer />
     </>
   );
 }
