53246f48da600d4578db59e4854c9562041cc1dd
LPRegen <manuel.escribano.051@gmail.com>
2023-07-26 13:26:07 +0200

    refactor: disable blog

--- app/(web)/blog/page.tsx
+++ app/(web)/blog/page.tsx
@@ -1,38 +1,39 @@
-import { CardPost } from "components/CardPost";
-import { Heading } from "components/Heading";
-import { getPosts } from "lib/queries";
+// import { CardPost } from "components/CardPost";
+// import { Heading } from "components/Heading";
+// import { getPosts } from "lib/queries";
 
 async function Blog() {
-  const [featPost, ...morePosts] = await getPosts();
+  // const [featPost, ...morePosts] = await getPosts();
   return (
     <>
-      <div className="mt-14 grid gap-8">
-        <Heading as="h1">Featured post</Heading>
-        <CardPost
-          slug={featPost.slug}
-          key={featPost._id}
-          title={featPost.title}
-          description={featPost.description}
-          variant="hero"
-          postImage={featPost.postImage}
-        />
-      </div>
-      <div className="grid gap-5 lg:grid-cols-2">
-        <Heading as="h2" className="mb-4 lg:col-span-full">
-          More posts
-        </Heading>
-        {morePosts.map((post) => {
-          return (
-            <CardPost
-              slug={post.slug}
-              key={post._id}
-              title={post.title}
-              description={post.description}
-              variant="regular"
-              postImage={post.postImage}
-            />
-          );
-        })}
+      <div className="mt-14 grid gap-8 h-[100vh]">
+        Under construction..
+        {/*   <Heading as="h1">Featured post</Heading> */}
+        {/*   <CardPost */}
+        {/*     slug={featPost.slug} */}
+        {/*     key={featPost._id} */}
+        {/*     title={featPost.title} */}
+        {/*     description={featPost.description} */}
+        {/*     variant="hero" */}
+        {/*     postImage={featPost.postImage} */}
+        {/*   /> */}
+        {/* </div> */}
+        {/* <div className="grid gap-5 lg:grid-cols-2"> */}
+        {/*   <Heading as="h2" className="mb-4 lg:col-span-full"> */}
+        {/*     More posts */}
+        {/*   </Heading> */}
+        {/*   {morePosts.map((post) => { */}
+        {/*     return ( */}
+        {/*       <CardPost */}
+        {/*         slug={post.slug} */}
+        {/*         key={post._id} */}
+        {/*         title={post.title} */}
+        {/*         description={post.description} */}
+        {/*         variant="regular" */}
+        {/*         postImage={post.postImage} */}
+        {/*       /> */}
+        {/*     ); */}
+        {/*   })} */}
       </div>
     </>
   );
