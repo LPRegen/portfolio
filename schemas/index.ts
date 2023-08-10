3fdea379c3ae480c879753119c557fffbae0c1c0
LPRegen <manuel.escribano.051@gmail.com>
2023-08-10 12:56:48 +0200

    style: format style

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,10 +2,6 @@ import category from "./category";
 import post from "./post";
 import project from "./projects";
 
-const schemas = [
-  category,
-  post,
-  project,
-];
+const schemas = [category, post, project];
 
 export default schemas;
