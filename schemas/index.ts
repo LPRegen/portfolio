802c389301bf4a5a0fb51f14b4f4dbcb01433493
LPRegen <manuel.escribano.051@gmail.com>
2023-08-10 20:02:59 +0200

    refactor: import projects schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,8 @@
 import category from "./category";
 import post from "./post";
 import project from "./project";
+import { projects } from "./projects";
 
-const schemas = [category, post, project];
+const schemas = [category, post, project, projects];
 
 export default schemas;
